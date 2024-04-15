293f8e6d993f76c701bae1b7a7d2152430aaded7
Azat <8280770+azataiot@users.noreply.github.com>
2024-04-15 12:55:04 +0300

    Flit cannot package module without docstring, or empty docstring

---
+++ djazz/__init__.py
@@ -0,0 +1,5 @@
+"""
+A ready-to-use Django project tailored for practical, real-world applications, djazz comes with pre-configured essential settings and industry best practices built-in, ensuring a streamlined development process right out of the box. Ideal for developers looking to jumpstart their projects without the hassle of initial setup.
+"""
+
+__version__ = "0.1.0-pre.1"
