6a05a91c5d9a70d7d4e12ef4e83a2218a278eb4b
Azat <8280770+azataiot@users.noreply.github.com>
2024-04-20 09:24:27 +0300

    fix #1 , indentation error.

--- djazz/settings.py
+++ djazz/settings.py
@@ -158,10 +158,10 @@ if DEBUG:
     # https://docs.djangoproject.com/en/5.0/ref/settings/#internal-ips
     import socket  # for docker development
 
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
-    "127.0.0.1",
-    "10.0.2.2",
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
+        "127.0.0.1",
+        "10.0.2.2",
 ]
 #
 
